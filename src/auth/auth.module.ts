7fbc922ac68df33337386945f3033c7bae21855e
TonimaDristy <tidristy477@gmail.com>
2025-12-05 02:44:13 +0600

    auth module

---
+++ src/auth/auth.module.ts
@@ -0,0 +1,10 @@
+import { Module } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { AuthController } from './auth.controller';
+
+@Module({
+    controllers: [AuthController],
+    providers: [AuthService],
+    exports: [AuthService], // optional, in case other modules need it
+})
+export class AuthModule { }
